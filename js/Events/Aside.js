33c8ef6048243fe9d0fe215eb7b8ea4ba20915a4
AtifWeb <mytechlife96@gmail.com>
2021-05-06 16:46:04 -0700

    DropDown Added

--- js/Events/Aside.js
+++ js/Events/Aside.js
@@ -3,6 +3,12 @@ const SalesContent = document.querySelector(".sales-contact");
 const SideBar = document.querySelector(".sidebar-wrapper");
 const BurgerMenu = document.querySelector(".burger-menu");
 const CloseIcon = document.querySelector(".sidebar-wrapper  .image-wrapper");
+const SideGroupHead=document.querySelector(".side-group-head")
+const SideGroupHeadArrow=document.querySelector(".side-group-head-arrow")
+const SideGroupContent=document.querySelector(".inner-setting")
+
+
+
 
 const HandleSideBarOpening = (e) => {
   SideBar.classList.add("active");
@@ -14,11 +20,17 @@ const GetDropDownFinanceDropDownHead = document.querySelector(
   ".finance-dropdown-head"
 );
 const FinanceContent = document.querySelector(".finance-dropdown-content");
+
 GetDropDownSalesHead.addEventListener("click", (e) => {
   SalesContent.classList.toggle("active");
 });
 GetDropDownFinanceDropDownHead.addEventListener("click", (e) => {
   FinanceContent.classList.toggle("active");
 });
+SideGroupHead.addEventListener("click",(e)=>{
+  SideGroupHeadArrow.classList.toggle("active");
+  SideGroupContent.classList.toggle("active");
+})
+
 BurgerMenu.addEventListener("click", HandleSideBarOpening);
 CloseIcon.addEventListener("click", HandleSideBarClosing);
